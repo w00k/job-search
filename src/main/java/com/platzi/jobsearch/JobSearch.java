a65d6d66dbeb0fbc9116013422c8ba7d46cb5200
Francisco Arenas <farenas@NI-NTB-120.local>
2021-12-20 21:32:49 -0300

    agrego funcionalidad de busqueda

--- src/main/java/com/platzi/jobsearch/JobSearch.java
+++ src/main/java/com/platzi/jobsearch/JobSearch.java
@@ -1,14 +1,47 @@
 package com.platzi.jobsearch;
 
 import com.beust.jcommander.JCommander;
+import com.google.gson.internal.bind.util.ISO8601Utils;
+import com.platzi.jobsearch.api.APIJobs;
 import com.platzi.jobsearch.cli.CLIArguments;
+import com.platzi.jobsearch.cli.CLIFunctions;
+import com.platzi.jobsearch.dto.JobPosition;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static com.platzi.jobsearch.api.APIFunctions.buildAPI;
 import static com.platzi.jobsearch.cli.CommanderFunctions.buildJCommanderWithName;
+import static com.platzi.jobsearch.cli.CommanderFunctions.parseArguments;
 
 public class JobSearch {
 
     public static void main(String[] args) {
-        System.out.println("hello your search");
-        JCommander jCommander = buildJCommanderWithName("job-search", CLIArguments::newInstance);;
+        System.out.println("Start job-search");
+        JCommander jCommander = buildJCommanderWithName("job-search", CLIArguments::newInstance);
+
+        Stream<CLIArguments> streamOfCLI = parseArguments(jCommander, args, JCommander::usage)
+                .orElse(Collections.emptyList())
+                .stream()
+                .map(object -> (CLIArguments) object);
+
+        Optional<CLIArguments> cliArgumentsOptional = streamOfCLI.filter(cli -> !cli.isHelp())
+                .filter(cli -> cli.getKeyword() != null)
+                .findFirst();
+
+        cliArgumentsOptional.map(CLIFunctions::toMap)
+                .map(JobSearch::executeRequest)
+                .orElse(Stream.empty())
+                .forEach(System.out::println);
+    }
+
+    private static Stream<JobPosition> executeRequest(Map<String, Object> params) {
+        APIJobs api = buildAPI(APIJobs.class, "https://jobs.github.com");
+        return Stream.of(params)
+                .map(api::jobs)
+                .flatMap(Collection::stream);
     }
 }
--- src/main/java/com/platzi/jobsearch/cli/CommanderFunctions.java
+++ src/main/java/com/platzi/jobsearch/cli/CommanderFunctions.java
@@ -1,14 +1,29 @@
 package com.platzi.jobsearch.cli;
 
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class CommanderFunctions {
 
     public static <T> JCommander buildJCommanderWithName(String cliname, Supplier<T> argumentSupplier) {
-        JCommander jCommander = new JCommander().newBuilder().addCommand(argumentSupplier.get()).build();
+        JCommander jCommander = new JCommander().newBuilder().addObject(argumentSupplier.get()).build();
         jCommander.setProgramName(cliname);
         return jCommander;
+    }
+
+    public static Optional<List<Object>> parseArguments(JCommander jCommander,
+            String[] arguments, Consumer<JCommander> onError) {
+        try {
+            jCommander.parse(arguments);
+            return Optional.of(jCommander.getObjects());
+        } catch (ParameterException parameterException) {
+            onError.accept(jCommander);
+        }
+        return Optional.empty();
     }
 }
